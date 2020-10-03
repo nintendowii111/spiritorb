b48080d33da7a380f1729478a382a0f918ce82d0
nintendowii111 <58327390+nintendowii111@users.noreply.github.com>
2020-10-03 03:16:07 -0500

    n/a

--- subsite_game/js/plugins/noDashOption.js
+++
@@ -1,4 +0,0 @@
-Window_Options.prototype.addGeneralOptions = function() {
-//  this.addCommand(TextManager.alwaysDash, 'alwaysDash');  // do not add this option
-    this.addCommand(TextManager.commandRemember, 'commandRemember');
-};
